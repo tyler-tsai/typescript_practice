aed6bd471f453d0f08997a03e4755e15dac10163
Tyler <15355143+tyler-tsai@users.noreply.github.com>
2019-01-17 11:21:46 +0800

    typescript with react and redux

---
+++ src/index.tsx
@@ -0,0 +1,23 @@
+import * as React from "react";
+import * as ReactDOM from "react-dom";
+import "./index.css";
+import Hello from "./containers/Hello";
+import { Provider } from "react-redux";
+import registerServiceWorker from "./registerServiceWorker";
+
+import { createStore } from "redux";
+import { enthusiasm } from "./reducers/index";
+import { StoreState } from "./types/index";
+import { EnthusiasmAction } from "./actions/index";
+const store = createStore<StoreState, EnthusiasmAction, any, any>(enthusiasm, {
+  enthusiasmLevel: 1,
+  languageName: "TypeScript"
+});
+
+ReactDOM.render(
+  <Provider store={store}>
+    <Hello />
+  </Provider>,
+  document.getElementById("root") as HTMLElement
+);
+registerServiceWorker();
